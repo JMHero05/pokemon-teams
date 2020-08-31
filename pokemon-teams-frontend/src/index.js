809962908df68ae1dc5be89be34f56de386c0f8e
Jake Mills <jake.mills.05@gmail.com>
2020-08-31 13:31:44 -0400

    Added trainers to DOM

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,32 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector('main')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const getTrainers = () => {
+    fetch(TRAINERS_URL)
+      .then(response => response.json())
+      .then(trainers => {
+        renderTrainers(trainers)
+      })
+  }
+
+  const renderTrainers = trainers => {
+    for (const trainer of trainers) {
+      renderTrainer(trainer)
+    }
+  }
+
+  const renderTrainer = trainer => {
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.dataset.id = trainer.id
+    div.textContent = trainer.name
+
+    main.append(div)
+  }
+
+  getTrainers()
+})
