c7de958ab51a47775ddba4414311d09cb982f494
humorous-storage-7521 <rmwerner@umich.edu>
2020-08-31 13:01:27 -0500

    make pokemons appear

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -24,8 +24,29 @@ document.addEventListener('DOMContentLoaded', () => {
     div.classList.add('card')
     div.dataset.id = trainer.id
     div.textContent = trainer.name
-
+    const ul = document.createElement('ul')
+    div.innerHTML = `
+    <button data-trainer-id=${trainer.id}>Add Pokemon</button>
+    `
+    div.append(ul)
+    displayPokemons(ul, trainer.pokemons)
+    console.log(trainer.pokemons)
     main.append(div)
+  }
+
+  const displayPokemons = (ul, pokemons) => {
+      for (const pokemon of pokemons) {
+        displayPokemon(ul, pokemon) 
+      }
+  }
+
+  const displayPokemon = (ul, pokemon) => {
+    const li = document.createElement('li')
+    li.innerHTML = `
+    ${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>
+    `
+    console.log(li)
+    ul.append(li)
   }
 
   getTrainers()
